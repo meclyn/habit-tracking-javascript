aa97cf096234393bcfddfad4f3e5b0e5f6f01abc
Matheus <matheusferreirasjc@gmail.com>
2024-05-31 01:38:22 -0300

    v1.07 Adicionado de volta o botão de remoção no HTML de hábitos

--- script.js
+++ script.js
@@ -47,21 +47,30 @@ function showValues(){
         const task = values[i];
         list.innerHTML += `
             <li>
-                ${task.name}
-                <button id='btn_${i}' class='btn_ok ${task.completed ? 'completed' : ''}' onclick='toggleTaskCompletion("${task.name}")'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
-                    </svg>
-                </button>
-            </li>`;
+            <button class="btn_remove" onclick="removeItem('${task.name}')">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                </svg>
+            </button>
+            ${task.name}
+            <button id='btn_${i}' class='btn_ok ${task.completed ? 'completed' : ''}' onclick='toggleTaskCompletion("${task.name}")'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                    <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                </svg>
+            </button>
+        </li>
+        `;
     }
 }
 
-function removeItem(data){
+function removeItem(taskName){
     let values = JSON.parse(localStorage.getItem(localStorageName) || '[]');
-    let index = valuesfin.findIndex(x => x.name == data)
-
-    
+    let index = values.findIndex(x => x.name == taskName);
+    if (index !== -1) {
+        values.splice(index, 1);
+        localStorage.setItem(localStorageName, JSON.stringify(values));
+        showValues();
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
